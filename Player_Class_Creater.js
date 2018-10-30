d3f47610f3d3770bd326d5f4c7b49d6079259772
2439905184 <suchenglong018@126.com>
2018-10-30 22:31:03 +0800

    再来

---
+++ Player_Class_Creater.js
@@ -0,0 +1,14 @@
+//声明一个Player类
+//一个角色有名字,生命,大招
+var Player=function(name,life,boom)
+{
+this.name=name
+this.life=life
+this.boom=function()
+{
+  //这里用canvas绘制大招特效
+}
+}
+//新建灵梦对象
+var LinMeng=new Player('LinMeng',3)
+test我就不信了
